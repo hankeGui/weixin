030deb3122304e54c15021daad48ebb6e3b2fd2b
hanke <hanke.gui@sap.com>
2022-08-13 23:16:14 +0800

    config

--- config.py
+++ config.py
@@ -1,20 +1,24 @@
 # 公众号配置
 # 公众号appId
-app_id = ""
+app_id = "wxa3a95bb8a11d26f6"
 # 公众号appSecret
-app_secret = ""
+app_secret = "cabbb2c0d5d3b8093096877b10d50b60"
 # 模板消息id
-# template_id = ""
+template_id = "q6bkfgm-YLnULGEnRNB_EH985YQpkMtFS0TZ81CLnVc"
 # 接收公众号消息的微信号
-user = ""
+# hanke
+user = "oGhoI6nm1IEfcOCr74fK15ZRs6tw"
+# 徐
+# user = "oGhoI6jAa_zqGi1G_ztZRuWXm5AE"
+
 
 # 信息配置
 # 所在省份
-province = "广东"
+province = "陕西"
 # 所在城市
-city = "广州"
+city = "西安"
 # 生日，如果月份或者日期小于10，直接用对应的数字即可，例如1997-1-1
 birthday = "1997-1-1"
 # 在一起的日子，格式同上
-love_date = "2022-1-1"
+love_date = "2022-8-10"
 
