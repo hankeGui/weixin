0cabf6d27db326daf62ed1380cbfad609437d7df
hanke <hanke.gui@sap.com>
2022-08-14 16:37:23 +0800

    fix weeklist bug

--- config.py
+++ config.py
@@ -11,15 +11,21 @@ PROVICE = 'PROVICE'
 # 公众号配置
 # 公众号appId
 
-# app_id = "wx0268dabce1770b63"
-app_id = os.environ(APP_ID)
+# app_id = "wxa3a95bb8a11d26f6"
+app_id = "wxa3a95bb8a11d26f6"
+if APP_ID in os.environ:
+    app_id = os.environ[APP_ID]
 # 公众号appSecret
-# app_secret = "e82c0bb576888426b1118dc17613d150"
-app_secret = os.environ(APP_SECRET)
+# app_secret = "cabbb2c0d5d3b8093096877b10d50b60"
+app_secret = "cabbb2c0d5d3b8093096877b10d50b60"
+if APP_SECRET in os.environ:
+    app_secret = os.environ[APP_SECRET]
 # 模板消息id
 # template_id = "q6bkfgm-YLnULGEnRNB_EH985YQpkMtFS0TZ81CLnVc"
 
-template_id = os.environ(TEMPLATE_ID)
+template_id = "q6bkfgm-YLnULGEnRNB_EH985YQpkMtFS0TZ81CLnVc"
+if TEMPLATE_ID in os.environ:
+    template_id = os.environ[TEMPLATE_ID]
 # 接收公众号消息的微信号
 
 # hanke
@@ -33,11 +39,19 @@ if USER_ID in os.environ:
 
 # 信息配置
 # 所在省份
-province = os.environ[PROVICE]
+province = "陕西"
+if PROVICE in os.environ:
+    province = os.environ[PROVICE]
 # 所在城市
-city = os.environ[CITY]
+city = "西安"
+if CITY in os.environ:
+    city = os.environ[CITY]
 # 生日，如果月份或者日期小于10，直接用对应的数字即可，例如1997-1-1
-birthday = os.environ[BIRTHDAY]
+birthday = "1997-1-1"
+# if BIRTHDAY in os.environ:
+#     birthday = os.environ[BIRTHDAY]
 # 在一起的日子，格式同上
-love_date = os.environ[START_DATE]
+love_date = "2022-8-10"
+# if START_DATE in os.environ:
+#     love_date = os.environ[START_DATE]
 
